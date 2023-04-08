2eef49aa5271cf4a2a9936494bbf9007a196207d
Nagendravarma1 <nagendravarmal512@gmail.com>
2023-04-08 10:46:29 +0530

    items added

---
+++ components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <h2>Expense item</h2>
+            <li>Food Rs 10</li>
+            <li>petrol Rs 100</li>
+            <li>Movies Rs 200</li>
+        </div>
+        
+    
+    )
+    
+}
+
+export default ExpenseItem;
