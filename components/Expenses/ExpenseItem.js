4fc6900a759192fb26ef340dc21d252ac6235d56
Nagendravarma1 <nagendravarmal512@gmail.com>
2023-04-11 19:27:07 +0530

    arrow functions

---
+++ components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        amount={props.amount}
+        location={props.location}
+      />
+    </Card>
+  );
+}
+
+export default ExpenseItem;
