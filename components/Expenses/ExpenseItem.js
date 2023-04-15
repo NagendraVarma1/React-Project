15236cca953be9b22d4055e31dfa60d29e9a738d
Nagendravarma1 <nagendravarmal512@gmail.com>
2023-04-15 09:58:41 +0530

    delete button

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,9 +1,19 @@
+import React, {useState} from "react";
 import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [isDeleted, setIsDeleted]= useState(false);
+  const deleteHandler = () => {
+    setIsDeleted(true);
+    console.log('Deleted');
+  };
+
+  if(isDeleted){
+    return null;
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,6 +22,7 @@ const ExpenseItem = (props) => {
         amount={props.amount}
         location={props.location}
       />
+      <button onClick={deleteHandler} className="btn">Delete Expenses</button>
     </Card>
   );
 }
