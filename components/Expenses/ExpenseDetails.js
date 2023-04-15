8fb12968d7c5f816fd1bfa1ac0c0cc613351398d
Nagendravarma1 <nagendravarmal512@gmail.com>
2023-04-15 20:29:28 +0530

    Amount Update

--- components/Expenses/ExpenseDetails.js
+++ components/Expenses/ExpenseDetails.js
@@ -1,9 +1,29 @@
+import React, {useState} from "react";
+
 const ExpenseDetails = (props) => {
+
+  const [title, setTitle] = useState(props.title)
+
+  const [amount, setAmount] = useState(props.amount)
+
+  const clickHandler = () => {
+    setTitle("Updated!!!")
+    console.log(title);
+    
+  }
+
+  const changeHandler = () => {
+    setAmount('100');
+    console.log(amount);
+  }
+
   return (
     <div className="expense-item__description">
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <div>{props.location}</div>
-      <div className="expense-item__price">${props.amount}</div>
+      <div className="expense-item__price">${amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={changeHandler}>Change Amount</button>
     </div>
   );
 }
